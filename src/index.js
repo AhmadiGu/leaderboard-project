20f8f807353cc760a84dbe4bf4bbb3e3749d0547
AhmadiGu <ahmadigulalai76@gmail.com>
2022-12-01 17:08:45 +0500

    Write code to get and post to api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,67 @@
 import './style.css';
+
+// const gameID = 'X9fxHdLZsiAYP8qUTFeL';
+
+const gameID = 'SAafjYwuy9TE9mm0ILLg';
+const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores/`;
+// https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/SAafjYwuy9TE9mm0ILLg/scores/
+
+const list = document.querySelector('.recent-content');
+const addForm = document.querySelector('.form');
+const listTable = document.querySelector('.table');
+const refreshBtn = document.querySelector('.refreshBtn');
+
+const displayInfo = (info) => {
+  listTable.innerHTML = '';
+  info.forEach((e) => {
+    listTable.innerHTML += `<tr>
+      <td class="user">${e.user}</td>
+      <td class="score">${e.score}</td>
+      </tr>`;
+  });
+};
+
+async function getInfo() {
+  await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json',
+    },
+  })
+    .then((respones) => respones.json())
+    .then((json) => displayInfo(json.result));
+}
+
+const postInfo = async (user, score) => {
+  await fetch(url,
+    {
+      method: 'POST',
+
+      body: JSON.stringify({
+        user,
+        score,
+      }),
+      headers: {
+        'Content-type': 'application/json',
+      },
+    })
+    .then((respones) => respones.json())
+    .then((data) => {
+      listTable.innerHTML = data.result;
+      list.appendChild(listTable);
+      getInfo();
+    });
+};
+
+addForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const name = addForm.user.value;
+  const scores = addForm.score.value;
+  postInfo(name, scores);
+  addForm.reset();
+});
+
+refreshBtn.addEventListener('click', async () => {
+  listTable.innerHTML = '';
+  await getInfo();
+});
